c31ca33faefb01276bea0533f14f59645b58ed45
Tomas Valent <equivalent@eq8.eu>
2015-03-23 10:19:26 +0000

    more explicit test

--- spec/missy_elliott_spec.rb
+++ spec/missy_elliott_spec.rb
@@ -8,5 +8,11 @@ describe "MissyElliott" do
     decoded = MissyElliott.decode(encoded)
     original.must_equal decoded
   end
+
+  it "Tests be more explicit" do
+    encoded = File.read './spec/fixtures/encode_example'
+    decoded = MissyElliott.decode(encoded)
+    decoded.must_equal "Is it really worth it?"
+  end
 end
 
